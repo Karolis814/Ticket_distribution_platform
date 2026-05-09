579e8e0f265faff27ce6276b5b16f063163c7890
roplyss <me@jsn.lt>
2026-05-09 11:55:49 +0300

    Add missing migrations

---
+++ src/TicketPlatform.Infrastructure/Persistence/Migrations/20260509074258_RefactoredEntityStructure.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketPlatform.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactoredEntityStructure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
